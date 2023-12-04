686e312e25bf3f9780e6a9df233867ed359d257f
Yousef Rastgar <rastgar.yousef@gmail.com>
2023-12-05 03:18:01 +0330

    added log to custom js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,8 @@ function customScript() {
 	window.addEventListener("DOMContentLoaded", () => {
 		const backToTopButton = document.getElementById("back-to-top");
 		if (backToTopButton) {
+			console.log("backToTopButton: ", backToTopButton);
+
 			backToTopButton.addEventListener("click", () => {
 				window.scrollTo({ top: 0 });
 			});
